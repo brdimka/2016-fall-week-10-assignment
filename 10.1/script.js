303183b12ca65c2346dda8ab6b8f7e917a1952d1
brdimka <briandimka@Brians-MacBook-Pro-2.local>
2016-11-17 20:06:29 -0500

    update

--- 10.1/script.js
+++ 10.1/script.js
@@ -12,13 +12,33 @@ var plot = d3.select('.canvas')
     .attr('transform','translate('+ m.l+','+ m.t+')');
 
 //Mapping specific functions
-//Projection
+//topojson.feature(data, data.objects.countries)
+var topoJson = d3.json('../data/world-50m.json',dataloaded);
 
-//Geopath
+topoJson.feature(data, data.objects.countries)
+var projection = d3.topoJson();
+  path = d3.topopath().projection(projection);
+
+var pathGenerator = d3.geopath()
+  .projection(projection);
 
 d3.json('../data/world-50m.json',dataloaded);
+
+function dataLoaded(err, data){
+  console.log(data);
+  projection
+    .fitExtent([[0,0],[w,h]],data);
+  }
+
+//projection.fitExtent([[0,0],[w,h]],geo);
+
+d3.json('//d3js.org/topojson.v1.min.js',dataloaded);
+
+
+
 
 function dataloaded(err, data){
     console.log(data); //This is a Topojson data
     console.log(topojson.feature(data,data.objects.countries)); //This is the converted GeoJSON data of countries
 }
+//topojson.feature(data, data.objects.countries)
